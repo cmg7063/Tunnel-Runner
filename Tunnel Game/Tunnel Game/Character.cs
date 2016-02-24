56d7e0ee2648ddc5a30d1531308d0a89f2f21ef0
Corinne Green <cmg7063@g.rit.edu>
2016-02-24 15:24:26 -0500

    Character base class for the abstract characters Norman and Kate.

---
+++ Tunnel Game/Tunnel Game/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnel_Game
+{
+    class Character
+    {
+        // Attributes
+        int health;
+        int speed;
+        string name;
+
+        // Constructor
+        public Character()
+        {
+
+        }
+
+        // Methods
+    }
+}
