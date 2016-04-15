5dcbab9622e786547f09bbec85475bc6f7c6d6dd
Corinne green <green.corinne8@live.com>
2016-04-15 16:53:10 -0400

    Lillian: Added a Levels class, haven't implemented yet

---
+++ TunnelRunner/TunnelRunner/Levels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TunnelRunner
+{
+    class Levels
+    {
+        Files allLevel = new Files();
+        public void Populate()
+        {
+            allLevel.LoadAllFile();
+        }
+        // this class will use data fron Files (fileInfo) to populate all lists
+        //to access fileInfo[0][0]  <------this is the first item in first level which would be what number of the level is
+    }
+}
